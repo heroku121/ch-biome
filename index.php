e123e193db2c866dfaf2f07d20f26afff632bc65
heroku121 <130366513+heroku121@users.noreply.github.com>
2023-05-16 19:07:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,266 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./images/favicon.png">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <title> LeanBiome</title>
+    <script type="text/javascript" src="https://bestofferhere.link/LeanBiomeHere"></script>
+
+
+</head>
+
+<body>
+
+    <div class="header ">
+        <div class="wrapper">
+            <nav style="text-align: center">
+                <h1 class="logo "> <a href="https://tryleanbiomeoffer.online/"><u> LeanBiome</u></a></h1>
+
+
+            </nav>
+        </div>
+    </div>
+
+    <div class="lsb-hero">
+        <div class="wrapper">
+            <div class="content fx-dr">
+                <div class="pr-img">
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="images/1.png" alt=""></a>
+                </div>
+                <div class="lsb-pr-text ">
+                    <h1>Support healthy weight loss with LeanBiome</h1>
+                    <a href="https://bestofferhere.link/LeanBiomeHere"> <img src="./images/star.png" style="max-width: 100px"></a>
+                    <p>LeanBiome\'s unique and patented formula combines nine species of clinically studied "lean bacteria" with Greenselect Phytosome, a next-generation green tea extract that has been formulated with Phytosome\'s patented absorption-boosting
+                        technology. </p>
+                    <a href="https://bestofferhere.link/LeanBiomeHere">
+                        <p style="color: #a00000;font-weight: 700;text-decoration: underline;cursor: pointer;">LeanBiome Official Website - Shop at Only $39</p>
+                    </a>
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/li-bt.png" style="max-width: 300px"></a>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class="lsb-abt ptb ">
+        <div class="wrapper">
+            <h1>What is LeanBiome ?</h1>
+            <p>
+                LeanBiome\'s unique and proprietary formula is the first in the world to combine nine clinically-researched \'lean bacteria\' species with Greenselect Phytosome®, a next-generation and caffeine-free green tea extract formulated with patented, absorption-boosting
+                Phytosome technology.</p>
+
+            <p>Together, this advanced ingredient blend helps to quickly reverse the bacterial imbalance in your gut microbiome – the newly-discovered root cause of belly fat and unexplained weight gain.</p>
+            <p> LeanBiome can help you reach your weight loss goals healthily and sustainably when used as directed. It is important to note that individual results may vary and that it is always a good idea to consult your doctor before starting any new
+                supplement regimen. Overall, if you are looking for a safe and natural way to support your weight loss journey, LeanBiome may be the right choice.</p>
+
+
+            <a href="https://bestofferhere.link/LeanBiomeHere"
+                style="color: #a00000;font-size: 22px;font-weight: 800;">Hurry Up!!! Order Now & You Save $540.00</a>
+        </div>
+    </div>
+    <div class="lsb-banner ptb ">
+        <div class="wrapper">
+            <h1>LeanBiome Benefits: What Does LeanBiome Do?</h1>
+        </div>
+    </div>
+
+    <div class="adv cent ptb">
+        <div class="wrapper">
+
+            <p>LeanBiome is a dietary supplement that aims to improve gut health and promote weight loss. By using a unique blend of natural ingredients, LeanBiome has several benefits beyond just weight loss.</p>
+            <div class="lsb-adv-li fx spc al-cn fx-dr">
+                <div class="lsb-adv-li-box">
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/adv1.png"></a>
+                    <h3>Improved Digestive Health</h3>
+                    <p> A healthy gut is essential for overall health, and LeanBiome can help improve digestive health.</p>
+                </div>
+                <div class="lsb-adv-li-box">
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/adv2.png"></a>
+                    <h3>Improved Heart Health</h3>
+                    <p>The ingredients in LeanBiome have been shown to improve heart health by reducing cholesterol levels.</p>
+                </div>
+                <div class="lsb-adv-li-box">
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/adv3.png"></a>
+                    <h3>Reduced Inflammation</h3>
+                    <p>The Greenselect phytosome in LeanBiome has been shown to reduce inflammation in the body.</p>
+                </div>
+            </div>
+
+            <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/li-bt.png" style="max-width: 300px;display: flex;
+                margin: auto;"></a>
+        </div>
+    </div>
+    <div class="lsb-bonuses ptb">
+        <div class="wrapper">
+            <div class="lsb-bonu cent ">
+
+                <h1 style="margin:10px 0">How It Works?</h1>
+                <p>LeanBiome is a unique weight loss supplement that adopts a fresh approach to fat reduction. It does not rely on metabolic stimulation or thermogenesis induction, which sets it apart from other products in the market.</p>
+            </div>
+            <div class=\'lsb-bonu-bt fx spc fx-dr\' style="margin-bottom: 30px">
+
+                <div class=\'lsb-bonu-bt-box-img \'>
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/work.jpg"></a>
+
+                </div>
+                <div class=\'lsb-bonu-bt-box-ittt  \'>
+                    <p>LeanBiome focuses on regulating the body\'s gut microbiome, which is recognized as a leading contributor to uncontrolled weight gain.</p>
+
+                    <p>The mechanism of LeanBiome involves reducing the absorption of calories in the body. </p>
+                    <p>Its formulation incorporates clinically proven plant extracts and probiotics.</p>
+                    <p>All of which are sourced from natural ingredients and aid in increased fat burning.</p>
+                    <p>A special blend of probiotic microbes present in LeanBiome is specifically designed to thrive within the gut, effectively repopulating the gut microbiota with an ideal balance.</p>
+                </div>
+            </div>
+        </div>
+        <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/li-bt.png" style="max-width: 300px;display: flex;
+                margin: auto;"></a>
+        <a href="https://bestofferhere.link/LeanBiomeHere"
+            style="color: #a00000;font-size: 22px;font-weight: 800;text-align: center;display: flex;justify-content: center">Hurry Up!!! Order Now & You Save $540.00</a>
+    </div>
+    <div class="why ptb">
+        <div class="wrapper">
+            <div style="">
+                <div class="lbs-why-inf fx spc  fx-dr">
+                    <div class="lbs-why-inf-img">
+                        <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/abt.jpg"></a>
+
+                    </div>
+                    <div class="lbs-why-inf-ctn ">
+                        <h4 style="color: red"></h4>
+                        <h1 style="margin:10px 0">Free Bonus when you order a 3 or 6 Month Supply</h1>
+                        <p>we’ll send you their brand-new eBook consisting of 21 gut-friendly smoothie recipes when you order a 3 or 6 month supply of LeanBiome.</p>
+                        <p> These smoothies take just a few minutes to prepare and taste great, making them a great option for a quick breakfast or afternoon snack! </p>
+                        <p>Let’s be clear: drinking these smoothies isn’t something you have to do! But if you’d like to accelerate the transformation in your gut microbiome, this is the easiest way to do it. You can even take LeanBiome with one of these
+                            smoothies each morning!</p>
+                        <a href="https://bestofferhere.link/LeanBiomeHere"
+                            style="color: #fff;font-size: 22px;font-weight: 800;text-align: center;display: flex">Hurry Up!!! Order Now & You Save $540.00</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class=\'gurst ptb\'>
+        <div class="wrapper">
+            <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/days.jpg" style=" max-width:  500px;display: flex;margin: auto"></a>
+            <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/li-bt.png" style="max-width: 300px;display: flex;
+                margin: auto;"></a>
+        </div>
+    </div>
+
+    <div class="inds ptb" style="background: #e9d1c5">
+        <div class="wrapper">
+            <div class="lbs-inds-hd cent ">
+
+                <h1 style="margin:10px 0">Our Key Research-Backed Ingredients
+                </h1>
+
+            </div>
+            <div class=\'lbs-inds-bt fx spc fx-dr\'>
+
+                <div class=\'lbs-inds-bt-box\'>
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/ind1.jpg"></a>
+                    <h3>LACTOBACILLUS GASSERI</h3>
+
+
+                </div>
+                <div class=\'lbs-inds-bt-box\'>
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/ind2.jpg"></a>
+                    <h3>LACTOBACILLUS RHAMNOSUS</h3>
+
+                </div>
+
+
+
+
+                <div class=\'lbs-inds-bt-box\'>
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/ing-3.jpg"></a>
+                    <h3>LACTOBACILLUS FERMENTUM </h3>
+
+                </div>
+                <div class=\'lbs-inds-bt-box\'>
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/ing-4.jpg"></a>
+                    <h3>GREENSELECT PHYTOSOME </h3>
+
+                </div>
+            </div>
+            <a href="https://bestofferhere.link/LeanBiomeHere"
+                style="color: #a00000;font-size: 22px;font-weight: 800;text-align: center;display: flex;justify-content: center">Hurry Up!!! Order Now & You Save $540.00</a>
+        </div>
+    </div>
+
+
+    <div class="lsb-Faq ptb">
+        <div class="wrapper">
+            <h1>Frequently Asked Question</h1>
+            <h2>How long will it take before I see results, and how much weight can I expect to lose??</h2>
+            <p>Of course, everyone is different, and will be starting out at different weights. Therefore, some people will inevitably lose more weight than others, and achieve it faster. To give you an idea of what to expect, consider the evidence: in a
+                double-blind, placebo-controlled and randomized study (the gold-standard of clinical trials) those taking Lactobacillus Gasseri lost a whopping 8.5% of belly fat in just 12 weeks. Even for the average American woman weighing 170 lbs, that’s
+                a loss of 15 lbs of belly fat in just three months – doing nothing else.</p>
+
+
+
+
+            <h2>Why are Sphaeranthus indicus, Garcinia mangostan and inulin also included in LeanBiome?</h2>
+            <p>We’re continually looking to improve upon the LeanBiome formula and give their customers even stronger results, so we’ve added three additional ingredients to the formula. Inulin is a dietary fiber that literally ‘feeds’ the lean bacteria
+                strains inside LeanBiome, helping to accelerate the transformation in your gut microbiome. Sphaeranthus indicus is a flowering plant native to India, and in combination with Garcinia mangostana, a fruit native to Malaysia and Indonesia,
+                has been clinically-shown in multiple studies to reduce belly fat and promote weight loss by reducing fat storage and accelerating fat burning.</p>
+
+            <h2>How many bottles should I order?</h2>
+            <p>To help you achieve visible and truly life-changing results (and save more money), we’ve created a special bundle plan consisting of 3-month and 6-month packages to go alongside the one-month starter option. That’s because clinical studies
+                of their key lean bacteria strains – Lactobacillus Gasseri, Rhamnosus and Fermentum – together with Greenselect Phytosome® show the longer you take LeanBiome, the more effective it becomes, and the more weight you lose. That’s why I personally
+                recommend stocking up on the 3-month or 6-month option while this special offer is running. This will not only save you money but ensure there’s no break in your progress so that you can enjoy the best possible fat loss results for the
+                long term. Plus, LeanBiome has a 2-year shelf life, so there’s no risk in stocking up now.</p>
+
+
+
+
+
+            <a href="https://bestofferhere.link/LeanBiomeHere"
+                style="color: #a00000;font-size: 22px;font-weight: 800;">Hurry Up!!! Order Now & You Save $540.00</a>
+        </div>
+    </div>
+
+    <div class="prcs ptb">
+        <div class=\'wrapper\'>
+            <h1 style="max-width: 900px;margin: 0 auto 30px;text-align: center"><a href="https://bestofferhere.link/LeanBiomeHere"
+                    style="color: #000"><u>Take advantage of this Special offer to stock up on LeanBiome while supplies last!</u></a></h1>
+            <div class="lsb-prcs-wrp  fx spc fx-dr" style="margin-bottom: 30px;text-align: center; ">
+                <div class="lsb-prcs-wrp-box">
+                    <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/p1.png"></a>
+                </div>
+                <div class="lsb-prcs-wrp-box">
+                    <a href="https://bestofferhere.link/LeanBiomeHere"> <img src="./images/p2.png"></a>
+                </div>
+                <div class="lsb-prcs-wrp-box">
+                    <a href="https://bestofferhere.link/LeanBiomeHere"> <img src="./images/p3.png"></a>
+                </div>
+            </div>
+            <a href="https://bestofferhere.link/LeanBiomeHere"><img src="./images/li-bt.png" style="max-width: 300px;display: flex;margin: auto;"></a>
+            <a href="https://bestofferhere.link/LeanBiomeHere"
+                style="color: #a00000;font-size: 22px;font-weight: 800;text-align: center;display: flex;justify-content: center">Hurry Up!!! Order Now & You Save $540.00</a>
+        </div>
+    </div>
+    <footer class="ptb" style="background: #e5886c;color: #fff;text-align: center">
+        <div class="wrapper">
+            <div class="inn-con txt-center">
+                <p>Copyright- LeanBiome 2023 | All Rights Reserved</p>
+
+                <div class="foot-link"><a href="">Privacy Policy </a>|<a href=""> Terms &amp; Conditions </a>|<a href="">contact us</a></div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
+';
+?>
